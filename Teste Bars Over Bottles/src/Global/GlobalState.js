dbd8dedebe19b50b595fdb96396c5eaca0c946df
Douglas Henrique <douggx10@Hotmail.com>
2022-08-24 14:41:55 -0300

    small fixes

--- Teste Bars Over Bottles/src/Global/GlobalState.js
+++ Teste Bars Over Bottles/src/Global/GlobalState.js
@@ -20,24 +20,6 @@ const GlobalState = (props) => {
 
   // const getProducts = () => {
   //   axios
-  //     .get(`https://bob-teste-front-end.herokuapp.com/api/products.json`, {
-  //       headers: {
-  //         "Content-Type": "application/json;charset=UTL-8",
-  //         "Access-Control-Allow-Origin": "true",
-  //         Accept: "application/json, text/plain, */*, application/jsonp",
-  //         "Access-Control-Allow-Headers": "Content-Type, Content-Length, Accept-Encoding"
-  //       }
-  //     }
-  //     )
-  //     .then(res => {
-  //       console.log(res)
-  //       setProducts(res.data)
-  //     })
-  //     .catch((err) => console.log(err))
-  // }
-
-  // const getProducts = () => {
-  //   axios
   //   .get(`https://cors-anywhere.herokuapp.com/https://bob-teste-front-end.herokuapp.com/api/products.json`)
   //   .then( res => {
   //     console.log(res)
@@ -49,6 +31,7 @@ const GlobalState = (props) => {
 
 
   // 
+  
   const states = {
     products, setProducts, currentProduct, setCurrentProduct, cart, setCart
   }
